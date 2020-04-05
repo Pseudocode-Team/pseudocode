512514259a2a91196f6e9f3c56c3465f81b4f415
Patryk Niedzwiedzinski <pniedzwiedzinski19@gmail.com>
2020-04-05 11:50:34 +0200

    Add sum

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,46 @@
+#include <stdio.h>
 #include <iostream>
 
+#define ADD 		'+'
+#define SUBSTRACT	'-'
+#define MULTIPLY	'*'
+#define DIVIDE		'/'
+
+struct ASTNode {
+	int value; //TODO: Dynamic typing, for now every value is int
+	void (*resolver)(int* acc, ASTNode* self, ASTNode* left, ASTNode* right);
+	ASTNode* left;
+	ASTNode* right;
+	void resolve(int* acc) {
+		resolver(acc, this, left, right);
+	}
+};
+
+void constIntResolver(int* acc, ASTNode* self, ASTNode* left, ASTNode* right) {
+	*acc = self->value;
+}
+
+ASTNode* createConstInt(int value) {
+	return new ASTNode{value, &constIntResolver, nullptr, nullptr};
+}
+
+void sumIntResolver(int* acc, ASTNode* self, ASTNode* left, ASTNode* right) {
+	left->resolve(acc);
+	int leftValue = *acc;
+	right->resolve(acc);
+	int rightValue = *acc;
+	*acc = leftValue + rightValue;
+}
+
+ASTNode* createSum(int a, int b) {
+	return new ASTNode{(int)NULL, &sumIntResolver, createConstInt(a), createConstInt(b)};
+}
+
+
 int main() {
-	std::cout << "Hello a World!" << std::endl;
+	int Accumulator;
+	ASTNode* program = createSum(1, 2);
+	program->resolve(&Accumulator);
+	std::cout << Accumulator;
 	return 0;
 }
