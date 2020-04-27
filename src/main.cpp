07d56d9b52d2f86f82b4fd120cad666bae1c1f17
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-04-27 12:20:54 +0200

    Fix tabs

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 #include <iostream>
 
-#define ADD 		'+'
+#define ADD 	'+'
 #define SUBSTRACT	'-'
 #define MULTIPLY	'*'
-#define DIVIDE		'/'
+#define DIVIDE	'/'
 
 //TODO: Dynamic typing, for now every value is int
 #define PseudoValue int // To enable dynamic typing we need to use one type for every value
